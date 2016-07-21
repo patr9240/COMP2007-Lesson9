9592d97f8d6c7523dc5e023e4eebccad0d1007cf
patr9240 <patr9240@gmail.com>
2016-07-21 13:59:01 -0400

    Add project files.

---
+++ COMP2007-Lesson9/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="COMP2007_Lesson9.MvcApplication" Language="C#" %>
---
+++ COMP2007-Lesson9/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(COMP2007_Lesson9.Startup))]
+namespace COMP2007_Lesson9
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
